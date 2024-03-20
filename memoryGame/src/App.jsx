7c1d15b094ccd77b12f6ce1cb58ce28e11e5b85d
Sintijaa <144904166+Sintijaa@users.noreply.github.com>
2024-03-20 10:14:55 +0200

    uzlabojums

--- memoryGame/src/App.jsx
+++ memoryGame/src/App.jsx
@@ -1,15 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import './App.css';
 
-function App() {
+const HomePage = () => (
+  <div>
+    <h1>Welcome to the Memory Game!</h1>
+    {/* Game components go here */}
+  </div>
+);
 
-  return (
-    <>
-      
-    </>
-  )
-}
+const RegistrationPage = () => (
+  <div>
+    <h1>Registration</h1>
+    {/* Registration form goes here */}
+  </div>
+);
 
-export default App
+const ProfilePage = () => (
+  <div>
+    <h1>Profile</h1>
+    {/* Profile details go here */}
+  </div>
+);
+
+const Header = () => (
+  <header>
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/register">Register</Link></li>
+        <li><Link to="/profile">Profile</Link></li>
+      </ul>
+    </nav>
+  </header>
+);
+
+const App = () => (
+  <Router>
+    <div>
+      <Header />
+      <Route exact path="/" component={HomePage} />
+      <Route path="/register" component={RegistrationPage} />
+      <Route path="/profile" component={ProfilePage} />
+    </div>
+  </Router>
+);
+
+export default App;
