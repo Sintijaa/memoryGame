692ec2e8aca03eb816946ee578ccedfb8c99b9fa
Sintijaa <144904166+Sintijaa@users.noreply.github.com>
2024-04-02 08:31:54 +0300

    .

--- memoryGame/src/components/Title.jsx
+++ memoryGame/src/components/Title.jsx
@@ -3,8 +3,7 @@ import '../style/title.css';
 export default function Title() {
     return (
         <div className="title">
-            <h1><span className="first-part">Colour </span>
-             <span className="second-part">Memo</span></h1>
+            <h1><span className="first-part">Colour MemoryGame </span></h1>
         </div>
     )
 }
